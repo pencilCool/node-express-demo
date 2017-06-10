47ce18b418370b313f798b5726e1be23360cfcf5
pencilCool <yhtangcoder@gmail.com>
2017-06-10 15:10:39 +0800

    路由

--- 2-chapter/helloWorld.js
+++ 2-chapter/helloWorld.js
@@ -1,5 +1,27 @@
 var http = require('http');
 http.createServer(function(req, res) {
+    // 规范化url， 去掉查询字符串，可选的反斜杠，并把它变成小写
+
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+    console.log(path)
+    switch (path) {
+        case '':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Homepage');
+            break;
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('About');
+            break;
+
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found')
+            break;
+
+
+    }
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello world!');
 }).listen(3000);
