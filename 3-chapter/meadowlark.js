fbedd4789e3545b14d1832deb390da2fb648a8ff
pencilCool <yhtangcoder@gmail.com>
2017-06-11 12:34:55 +0800

    图片正常显示

--- 3-chapter/meadowlark.js
+++ 3-chapter/meadowlark.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+// static 中间件 相当于给你想要发送的所有静态文件创建了一个路由
+app.use(express.static(__dirname + '/public'));
+
+
 //< 这段代码创建了一个视图引擎，并对 Express 进行了配置，将其作为默认的视图引擎
 var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
@@ -9,6 +13,15 @@ app.set('view engine', 'handlebars');
 
 
 app.set('port', process.env.PORT || 3000);
+
+
+
+
+
+
+
+
+
 
 app.get('/', function(req, res) {
     res.render('home');
