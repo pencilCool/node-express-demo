6c0cd0bf1299600491e2a847469c6d713d1019ee
pencilCool <yhtangcoder@gmail.com>
2017-06-14 10:59:27 +0800

    tmp

---
+++ documentWrite.js
@@ -0,0 +1,4 @@
+document.write('<h1>Please Don\'t Do This</h1>');
+document.write('<p><span class="code">document.write</span> is naughty,\n');
+document.write('and should be avoided at all costs.</p>');
+document.write('<p>Today\'s date is ' + new Date() + '.</p>');
